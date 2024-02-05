8a409e3821d74d2b304ca37eb734e3a4567af5b4
MOHAMMED ELAMINE TAHAR BENGUELLA <107438222+thrbng@users.noreply.github.com>
2024-02-05 23:49:52 +0100

    Update ConnectivityReciver.kt

--- Classes/ConnectivityReciver.kt
+++ Classes/ConnectivityReciver.kt
@@ -1,7 +1,5 @@
 @file:Suppress("DEPRECATION")
 
-package com.example.stockshaper.baseActivityInternet
-
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
